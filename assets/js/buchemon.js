69564acf659482d746659f3f1f8aaeeca1acd74e
Alejo <Alejandrovanegas09@outlook.com>
2023-04-03 17:14:57 -0500

    Añade metodos para los ataques de cada jugador, y la definicion de un combate

--- assets/js/buchemon.js
+++ assets/js/buchemon.js
@@ -1,6 +1,16 @@
+let ataqueJugador;
+let ataquePC;
+
 function gameDevelop(){
     let btnSeleccion = document.getElementById('seleccion--buche');
     btnSeleccion.addEventListener('click',seleccionBuchemon);
+    let btnFuego = document.getElementById('ataqueFuego');
+    btnFuego.addEventListener('click',ataqueFuego);
+    let btnAgua = document.getElementById('ataqueAgua');
+    btnAgua.addEventListener('click',ataqueAgua);
+    let btnTierra = document.getElementById('ataqueTierra');
+    btnTierra.addEventListener('click',ataqueTierra); 
+
 }
 
 function seleccionBuchemon(){
@@ -40,6 +50,57 @@ function seleccionBuchemonEnemigo(){
 
 function alet(min, max){
     return rand=Math.floor(Math.random()*(max-min+1)+1);
+}
+
+function ataqueFuego(){
+    ataqueJugador='Fuego';
+    ataqueEnemigo();
+}
+
+function ataqueAgua(){
+    ataqueJugador='Agua';
+    ataqueEnemigo();
+}
+
+function ataqueTierra(){
+    ataqueJugador='Tierra';
+    ataqueEnemigo();
+}
+
+function ataqueEnemigo(){
+    let ataqueAleatorioEnemigo=alet(1,3);
+    let spanAtaqueEnemigo=document.getElementById('');
+    if(ataqueAleatorioEnemigo==1){
+        ataquePC='Fuego';
+    }   
+    else if(ataqueAleatorioEnemigo==2){
+        ataquePC="Agua";
+    }
+    else{
+        ataquePC="Tierra";
+    }
+
+    fedback();
+}
+
+function fedback(){
+    let sectionMensaje=document.getElementById('feedback');
+    let parrafo=document.createElement('p');
+    parrafo.innerHTML='Tu buchemon ataco con '+ataqueJugador+'. El buchemon enemigo ataco con '+ataquePC+' - '+playing()+' !';
+
+    sectionMensaje.appendChild(parrafo)
+}
+
+function playing(jugador, maquina){
+    if ((ataqueJugador == 'Fuego' && ataquePC == 'Tierra') || (ataqueJugador == 'Agua' && ataquePC == 'Fuego') || (ataqueJugador == 'Tierra' && ataquePC == 'Agua')) {
+        return 'GANASTE'
+    }
+    else if (ataqueJugador == ataquePC) {
+        return 'EMPATE'
+    } 
+    else {
+        return 'PERDISTE'
+    }
 }
 
 window.addEventListener('load',gameDevelop);
